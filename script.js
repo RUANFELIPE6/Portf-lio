e547043ef9a25a351b2b737ba47893d1fa72fb66
RuanFelipe <renanfruck525@gmail.com>
2025-07-29 21:08:53 -0300

    Primeiro commit

---
+++ script.js
@@ -0,0 +1,45 @@
+
+particlesJS("particles-js", {
+  particles: {
+    number: {
+      value: 80,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    },
+    color: {
+      value: "#ffffff"
+    },
+    shape: {
+      type: "circle"
+    },
+    opacity: {
+      value: 0.5
+    },
+    size: {
+      value: 3
+    },
+    move: {
+      enable: true,
+      speed: 0.3 // movimento lento
+    }
+  },
+  interactivity: {
+    detect_on: "window",
+    events: {
+      onhover: {
+        enable: true,
+        mode: "repulse"
+      }
+    },
+    modes: {
+      repulse: {
+        distance: 40,   // ← menor distância do efeito
+        duration: 0.3  // ← duração menor, movimento mais sutil
+      }
+    }
+  },
+  retina_detect: true
+});
+
